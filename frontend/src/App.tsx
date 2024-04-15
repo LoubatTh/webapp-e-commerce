e7d107b72e9da00205425aff10b5365dbdfa35f7
DMatteo <mdegano@webdev.com>
2024-04-15 12:14:25 +0200

    init frontend   - check for jest implementation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
