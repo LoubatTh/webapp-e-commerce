fbf60b720be9ac097e86bc340961ec70f17c0c7a
Ariirau FUCKS <ariirau.fucks@epitech.eu>
2024-04-23 16:00:54 +0200

    refactor: set correct path for symfony

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" == apache2* ]] || [ "$1" = 'php-fpm' ]; then
     echo "INSTALLING DEPENDENCIES with composer install"
-    cd /usr/src/app && composer install
+    cd /usr/src && composer install
 fi
 
 echo "STARTING APACHE"
