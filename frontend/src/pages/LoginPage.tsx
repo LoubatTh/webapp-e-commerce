21ec829f0ad5a1f86d9147a0c315c70bc4dc2017
DMatteo <mdegano@webdev.com>
2024-04-30 14:08:38 +0200

    adding start login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,55 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import LoginForm from "@/components/login/loginForm";
 
-const apiRegister = async (username: string, password: string) => {
-  const response = await fetch("http://localhost:5000/api/auth/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password,
-    }),
-  });
-  const data = await response.json();
-  console.log(data);
-};
-
-const apiLogin = async (username: string, password: string) => {
-  const response = await fetch("http://localhost:5000/api/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password,
-    }),
-  });
-  const data = await response.json();
-  console.log(data);
-};
 const LoginPage = () => {
-  const username = "matteo";
-  const password = "matteoPassword";
-
-  const register = async () => {
-    apiRegister(username, password);
-  };
-
-  const login = async () => {
-    apiLogin(username, password);
-  };
-
   return (
     <>
-      <h1>auth</h1>
-      <div>
-        <Button onClick={register}>Register</Button>
-      </div>
-      <div>
-        <Button onClick={login}>Login</Button>
-      </div>
+      <Tabs defaultValue="account" className="w-[800px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Register</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login" className="p-2">
+          <LoginForm />
+        </TabsContent>
+        <TabsContent value="register"></TabsContent>
+      </Tabs>
     </>
   );
 };
