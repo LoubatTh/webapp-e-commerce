fe58248b36fef63c731ddd9777081fe1224aa9af
LoubatTh <thomasloubat1@gmail.com>
2024-05-11 01:00:09 +0200

    feat: add product relations and fixtures

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Brand;
+use App\Entity\Gender;
+use App\Entity\Size;
+use App\Entity\Type;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +13,54 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $brands = array("Nike", "Adidas", "Lacoste", "Under Armor", "Calvin Klein", "Champion", "The North Face");
+        $this->createBrands($manager, $brands);
+
+        $genders = array("Men", "Women", "Kid");
+        $this->createGenders($manager, $genders);
+
+        $size = array("XS", "S", "M", "L", "XL", "XXL", "XXXL", "∞L");
+        $this->createSize($manager, $size);
+
+        $type = array("Shirt", "Sweat", "Short", "Pants", "Shoes");
+        $this->createType($manager, $type);
 
         $manager->flush();
+    }
+
+    private function createBrands(ObjectManager $manager, array $brands): void
+    {
+        foreach ($brands as $brandName) {
+            $brand = new Brand();
+            $brand->setName($brandName);
+            $manager->persist($brand);
+        }
+    }
+
+    private function createGenders(ObjectManager $manager, array $genders): void
+    {
+        foreach ($genders as $genderName) {
+            $gender = new Gender();
+            $gender->setGender($genderName);
+            $manager->persist($gender);
+        }
+    }
+
+    private function createSize(ObjectManager $manager, array $size): void
+    {
+        foreach ($size as $sizeName) {
+            $size = new Size();
+            $size->setSize($sizeName);
+            $manager->persist($size);
+        }
+    }
+
+    private function createType(ObjectManager $manager, array $type): void
+    {
+        foreach ($type as $typeName) {
+            $type = new Type();
+            $type->setType($typeName);
+            $manager->persist($type);
+        }
     }
 }
---
+++ app/src/Entity/Size.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SizeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SizeRepository::class)]
+class Size
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $size = null;
+
+    /**
+     * @var Collection<int, Product>
+     */
+    #[ORM\ManyToMany(targetEntity: Product::class, mappedBy: 'Size')]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSize(): ?string
+    {
+        return $this->size;
+    }
+
+    public function setSize(string $size): static
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->addSize($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        if ($this->products->removeElement($product)) {
+            $product->removeSize($this);
+        }
+
+        return $this;
+    }
+}
