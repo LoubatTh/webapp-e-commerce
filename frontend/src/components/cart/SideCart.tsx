c46a30bb33e900c6a67612af1f9ff240c499ddf1
DMatteo <mdegano@webdev.com>
2024-05-13 10:29:40 +0200

    ended sidecart

--- frontend/src/components/cart/SideCart.tsx
+++ frontend/src/components/cart/SideCart.tsx
@@ -1,19 +1,39 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { useCartStore } from "@/lib/store/cartStore";
 import ProductCardSmall from "../productCard/ProductCardSmall";
+import { ScrollArea } from "../ui/scroll-area";
+import { useEffect, useState } from "react";
 
 export const SideCart = () => {
   const { items, clearCart } = useCartStore();
+  const [total, setTotal] = useState(0);
+  const totalCart = () => {
+    let total = 0;
+    items.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    setTotal(total);
+  };
+  useEffect(() => {
+    totalCart();
+  }, [items]);
 
   return (
-    <div className="flex flex-col item-center">
-      <button className="uppercase my-4" onClick={clearCart}>
-        Your Cart
-      </button>
-      <div className="flex flex-col gap-4">
-        {items.map((item) => (
-          <ProductCardSmall key={item.id} product={item} />
-        ))}
+    <div className="w-72 border-l shadow-border shadow-md fixed h-screen right-0 top-[41px] justify-between">
+      <ScrollArea className="flex flex-col text-center justify-center max-h-[calc(100vh-110px)] h-[calc(100vh-60px)] w-full p-4">
+        <div className="flex flex-col item-center">
+          <button className="uppercase my-4" onClick={clearCart}>
+            Your Cart
+          </button>
+          <div className="flex flex-col gap-4">
+            {items.map((item) => (
+              <ProductCardSmall key={item.id} product={item} />
+            ))}
+          </div>
+        </div>
+      </ScrollArea>
+      <div className="flex flex-col justify-center items-center w-72 fixed border-l border-t shadow-border shadow-md right-0 bottom-0 pb-3 bg-background">
+        <div>Total: {total}€</div>
+        <button className="w-2/3 bg-primary text-white py-2">Purchase</button>
       </div>
     </div>
   );
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -2,7 +2,6 @@ import { products } from "@/lib/DUMMY-DATA/products";
 import ProductCardMedium from "@/components/productCard/ProductCardMedium";
 import { useCartStore } from "@/lib/store/cartStore";
 import { SideCart } from "@/components/cart/SideCart";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Homepage = () => {
   const { items } = useCartStore();
@@ -23,19 +22,7 @@ const Homepage = () => {
           ))}
         </div>
       </div>
-      {!isEmpty && (
-        <div className="w-72 border-l shadow-border shadow-md fixed h-screen right-0 top-10 justify-between">
-          <ScrollArea className="flex flex-col text-center justify-center max-h-[calc(100vh-110px)] h-[calc(100vh-60px)] w-full p-4">
-            <SideCart />
-          </ScrollArea>
-          <div className="flex flex-col justify-center items-center w-72 fixed border-l border-t shadow-border shadow-md right-0 bottom-0 pb-3 bg-background">
-            <div>Total: </div>
-            <button className="w-2/3 bg-primary text-white py-2">
-              Purchase
-            </button>
-          </div>
-        </div>
-      )}
+      {!isEmpty && <SideCart />}
     </div>
   );
 };
