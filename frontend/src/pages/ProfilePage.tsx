41a54fbd01a3647d4e632940b737aa7c975de36c
Matteo-Degano <mdegano@webdev.com>
2024-05-14 17:27:57 +0200

    fixed issue with role

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { fetchApiPrivate } from "@/lib/apiPrivate";
+import { useEffect, useState } from "react";
+
+const getUserApi = async () => {
+  const response = await fetchApiPrivate("GET", "user");
+  return response;
+};
 
 const ProfilePage = () => {
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const actualUrl = window.location.href;
+  const [isAdmin, setIsAdmin] = useState(false);
 
   const LogOut = () => {
     Cookies.remove("authToken");
-    setIsLoggedIn(false);
     navigate("/");
   };
 
-  useEffect(() => {
-    const token = Cookies.get("authToken");
-    const userIsLoggedIn = !!token;
-    setIsLoggedIn(userIsLoggedIn);
-  }, [actualUrl]);
+  const getUser = async () => {
+    const response = await getUserApi();
+    const role = response.data.roles[0];
+    if (role === "ROLE_ADMIN") {
+      setIsAdmin(true);
+    }
+  };
 
-  if (isLoggedIn) {
-    return (
-      <div>
-        <Button onClick={LogOut}>Logout</Button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Profile</h1>
-        <p>You are not logged in</p>
-      </div>
-    );
-  }
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <div>
+      {isAdmin && (
+        <Button onClick={() => navigate("/admin")}>Back office</Button>
+      )}
+      <Button onClick={LogOut}>Logout</Button>
+    </div>
+  );
 };
 
 export default ProfilePage;
